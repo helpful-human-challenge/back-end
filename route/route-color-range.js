4e9e8630ee2dc5be62b2fe47e5c7a7fcb799c527
Melanie Downing <sayanything830@gmail.com>
2018-06-09 18:21:35 -0700

    working on adding many colors to database

--- route/route-color-range.js
+++ route/route-color-range.js
@@ -7,6 +7,7 @@ const errorHandler = require('../middleware/error-handler');
 module.exports = function(router) {
   router.route('/range/:color?')
     .get((request, response) => {
+      log('__ROUTE_GET_RANGE__');
       if(request.params.color) {
         return Color.find({
           colorRange: request.params.color,
